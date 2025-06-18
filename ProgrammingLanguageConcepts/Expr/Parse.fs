754ec1743d5669959fb72822f4e2573cf73c65e4
teeaychem <ben@sparkes.uk.net>
2025-06-18 22:47:11 +0100

    Expr example files
    Along with FsLexYacc and some tests roughly corresponding to Exercise 3.5

---
+++ ProgrammingLanguageConcepts/Expr/Parse.fs
@@ -0,0 +1,31 @@
+(* File Expr/Parse.fs *)
+(* Lexing and parsing of simple expressions using fslex and fsyacc *)
+
+module Parse
+
+open System.IO
+open FSharp.Text.Lexing
+open Absyn
+
+(* Plain parsing from a string, with poor error reporting *)
+
+let fromString (str: string) : expr =
+    let lexbuf = LexBuffer<char>.FromString str
+
+    try
+        ExprPar.Main ExprLex.Token lexbuf
+    with exn ->
+        let pos = lexbuf.EndPos
+        failwithf "%s near line %d, column %d\n" exn.Message (pos.Line + 1) pos.Column
+
+(* Parsing from a text file *)
+
+let fromFile (filename: string) =
+    use reader = new StreamReader(filename)
+    let lexbuf = LexBuffer<char>.FromTextReader reader
+
+    try
+        ExprPar.Main ExprLex.Token lexbuf
+    with exn ->
+        let pos = lexbuf.EndPos
+        failwithf "%s in file %s near line %d, column %d\n" exn.Message filename (pos.Line + 1) pos.Column
