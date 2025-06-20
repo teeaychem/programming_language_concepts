00eac93d9b028f55aec29763fdd8e2052d03d369
teeaychem <bsparkes@stanford.edu>
2025-06-20 09:38:59 +0100

    Exercise 5.3, merging lists
    Mostly simple.
    
    Mostly, as type inference for functional parameters in C++ isn't so clear to me.
    
    I expect these exercises are primarily designed as introductions to function passing.
    And, in this respect, these are lessons already learnt --- though likely for other reads too given the amount of F# inspected so far.

--- ProgrammingLanguageConcepts.Tests/higherfun.cpp
+++ ProgrammingLanguageConcepts.Tests/higherfun.cpp
@@ -1,4 +1,6 @@
 #include <cstdio>
+#include <functional>
+#include <sstream>
 #include <string>
 #include <vector>
 
@@ -40,25 +42,25 @@ public:
   }
 
   std::string to_string() {
-    std::string str{};
+    std::stringstream ss{};
 
     Node<T> *curr = first;
 
     if (curr == nullptr) {
-      return str;
+      return ss.str();
     }
 
     while (true) {
-      str.append(std::to_string(curr->item()));
+      ss << curr->item();
       curr = curr->next();
       if (curr != nullptr) {
-        str.push_back(' ');
+        ss << ' ';
       } else {
         break;
       }
     }
 
-    return str;
+    return ss.str();
   }
 };
 
@@ -92,8 +94,7 @@ std::vector<int> merge_sorted_arrays(std::vector<int> const &a,
 }
 
 // Exercise 5.1
-LinkedList<int> merge_sorted_lists(LinkedList<int> const &a,
-                                        LinkedList<int> const &b) {
+LinkedList<int> merge(LinkedList<int> const &a, LinkedList<int> const &b) {
 
   Node<int> *ah = a.head();
   Node<int> *bh = b.head();
@@ -123,7 +124,70 @@ LinkedList<int> merge_sorted_lists(LinkedList<int> const &a,
   return out;
 }
 
-void merge_sorted_arrays_test() {
+template <typename T>
+LinkedList<T> mergep(LinkedList<T> const &a, LinkedList<T> const &b) {
+
+  Node<T> *ah = a.head();
+  Node<T> *bh = b.head();
+
+  LinkedList<T> out{};
+
+  while (ah != nullptr && bh != nullptr) {
+    if (ah->item() < bh->item()) {
+      out.add(ah->item());
+      ah = ah->next();
+    } else {
+      out.add(bh->item());
+      bh = bh->next();
+    }
+  }
+
+  while (ah != nullptr) {
+    out.add(ah->item());
+    ah = ah->next();
+  }
+
+  while (bh != nullptr) {
+    out.add(bh->item());
+    bh = bh->next();
+  }
+
+  return out;
+}
+
+template <typename T>
+LinkedList<T> mergec(LinkedList<T> const &a, LinkedList<T> const &b,
+                     std::function<int(T, T)> cmp) {
+
+  Node<T> *ah = a.head();
+  Node<T> *bh = b.head();
+
+  LinkedList<T> out{};
+
+  while (ah != nullptr && bh != nullptr) {
+    if (cmp(ah->item(), bh->item()) < 1) {
+      out.add(ah->item());
+      ah = ah->next();
+    } else {
+      out.add(bh->item());
+      bh = bh->next();
+    }
+  }
+
+  while (ah != nullptr) {
+    out.add(ah->item());
+    ah = ah->next();
+  }
+
+  while (bh != nullptr) {
+    out.add(bh->item());
+    bh = bh->next();
+  }
+
+  return out;
+}
+
+void five_point_one() {
   std::vector<int> xs{3, 5, 12};
   std::vector<int> ys{2, 3, 4, 7};
 
@@ -135,7 +199,7 @@ void merge_sorted_arrays_test() {
   printf("\n");
 }
 
-void merge_sorted_lists_test() {
+void five_point_two() {
   LinkedList<int> a{};
   a.add(3);
   a.add(5);
@@ -147,14 +211,59 @@ void merge_sorted_lists_test() {
   b.add(4);
   b.add(7);
 
-  LinkedList<int> merged = merge_sorted_lists(a, b);
+  LinkedList<int> merged = merge(a, b);
 
   printf("a: [%s], b: [%s], sorted: [%s]\n", a.to_string().c_str(),
          b.to_string().c_str(), merged.to_string().c_str());
 }
 
+void five_point_three_b() {
+
+  LinkedList<std::string> a{};
+
+  a.add("abc");
+  a.add("apricot");
+  a.add("ballad");
+  a.add("zebra");
+
+  LinkedList<std::string> b{};
+  b.add("abelian");
+  b.add("ape");
+  b.add("carbon");
+  b.add("yosemite");
+
+  LinkedList<std::string> mergedp = mergep(a, b);
+
+  printf("a: [%s], b: [%s], sorted: [%s]\n", a.to_string().c_str(),
+         b.to_string().c_str(), mergedp.to_string().c_str());
+}
+
+void five_point_three_c() {
+  LinkedList<int> a{};
+  a.add(3);
+  a.add(5);
+  a.add(12);
+
+  LinkedList<int> b{};
+  b.add(2);
+  b.add(3);
+  b.add(4);
+  b.add(7);
+
+  std::function<int(int, int)> icmp = [](int x, int y) {
+    return x < y ? -1 : (x > y ? 1 : 0);
+  };
+
+  LinkedList<int> mergedc = mergec(a, b, icmp);
+
+  printf("a: [%s], b: [%s], sorted: [%s]\n", a.to_string().c_str(),
+         b.to_string().c_str(), mergedc.to_string().c_str());
+}
+
 int main(int argc, char *argv[]) {
 
-  merge_sorted_arrays_test();
-  merge_sorted_lists_test();
+  five_point_one();
+  five_point_two();
+  five_point_three_b();
+  five_point_three_c();
 }
