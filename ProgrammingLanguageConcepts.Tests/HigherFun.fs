c909e61f8874e8b63856205b8b8c7295b619d0f3
teeaychem <ben@sparkes.uk.net>
2025-06-20 19:35:08 +0100

    Exercise 6.1, closures
    Loading the wrong parser led to some confusion.
    
    Of note, the implementation of multi-arg functions requires disambiguation of the first expression.
    And, explaining closures is harder than expected.
    
    Otherwise, no notes.

--- ProgrammingLanguageConcepts.Tests/HigherFun.fs
+++ ProgrammingLanguageConcepts.Tests/HigherFun.fs
@@ -3,6 +3,7 @@ module TestsHigherFun
 
 open Xunit
 
+open FunParse
 open FunAbsyn
 open HigherFun
 
@@ -35,7 +36,7 @@ let ``setup`` () =
             Letfun("mul3", [ "y" ], Prim("*", CstI 3, Var "y"), Call(Call(Var "tw", [ Var "mul3" ]), [ CstI 11 ]))
         )
 
-    let ex4 =
+    let _ex4 =
         Letfun(
             "tw",
             [ "g" ],
@@ -48,3 +49,45 @@ let ``setup`` () =
     Assert.Equal(Int 3628800, eval ex2 [("n", Int 10)])
     Assert.Equal(Int 99, eval ex3 [])
     // Assert.Equal(..., eval ex4 [])
+
+
+[<Fact>]
+let ``Exercise 6.1`` () =
+    let e1 = fromString "
+let add x = let f y = x+y in f end in (add 2) 5 end
+"
+    printfn "%A" e1
+
+    let e2 = fromString "
+let add x = let f y = x+y in f end
+in let addtwo = add 2
+    in addtwo 5 end
+end"
+
+    let e3 = fromString "let add x = let f y = x+y in f end
+in let addtwo = add 2
+in let x = 77 in addtwo 5 end
+end
+end"
+
+// let add x = let f y = x + y in f
+// let addTwo = add 2
+// add 2 >> let f y = 2 + y in f >> f y
+// addtwo 5 >> f 5 >> 7
+
+// addtwo 5
+// (add 2) 5
+// f 5
+// 2 + 5
+
+// etc.
+
+    let _e4 = fromString "let add x = let f y = x+y in f end
+in add 2 end"
+
+    Assert.Equal(Int 7, eval e1 [])
+    Assert.Equal(Int 7, eval e2 [])
+    Assert.Equal(Int 7, eval e3 [])
+    // Assert.Equal(..., eval e4 [])
+
+    true
