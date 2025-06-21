0bde3c25d2970d9a1559f7590a1c5a8f4d05ff5f
teeaychem <ben@sparkes.uk.net>
2025-06-21 13:17:21 +0100

    Additional tests from ParseAndRunHigher
    Adapted for multi-arg funs, and with ex9 omitted as it's ex8 with some disambiguation.

--- ProgrammingLanguageConcepts.Tests/HigherFun.fs
+++ ProgrammingLanguageConcepts.Tests/HigherFun.fs
@@ -52,6 +52,66 @@ let ``setup`` () =
 
 
 [<Fact>]
+let ``parse and run`` () =
+    let ex5 =
+        fromString
+            @"let tw g = let app x = g (g x) in app end
+       in let mul3 x = 3 * x
+       in let quad = tw mul3
+       in quad 7 end end end"
+
+    let ex6 =
+        fromString
+            @"let tw g = let app x = g (g x) in app end
+       in let mul3 x = 3 * x
+       in let quad = tw mul3
+       in quad end end end"
+
+    let ex7 =
+        fromString
+            @"let rep n =
+           let rep1 g =
+               let rep2 x = if n=0 then x else ((rep (n-1)) g) (g x)
+               in rep2 end
+           in rep1 end
+       in let mul3 x = 3 * x
+       in let tw = rep 2
+       in let quad = tw mul3
+       in quad 7 end end end end"
+
+    let ex8 =
+        fromString
+            @"let rep n =
+           let rep1 g =
+               let rep2 x = if n=0 then x else ((rep (n-1)) g) (g x)
+               in rep2 end
+           in rep1 end
+       in let mul3 x = 3 * x
+       in let twototen = (rep 10) mul3
+       in twototen 7 end end end"
+
+    let ex8sharp =
+        let rec rep n =
+            let rep1 g =
+                let rep2 x =
+                    if n = 0 then x else rep (n - 1) g (g x) in
+
+                rep2 in
+
+            rep1 in
+
+        let mul3 x = 3 * x in
+        let twototen = rep 10 mul3 in
+        twototen 7
+
+
+    Assert.Equal(Int 63, eval ex5 [])
+    Assert.Equal(Int 63, eval (Call(ex6, [ CstI 7 ])) [])
+    Assert.Equal(Int 63, eval ex7 [])
+    Assert.Equal(Int ex8sharp, eval ex8 [])
+
+
+[<Fact>]
 let ``Exercise 6.1`` () =
     let e1 =
         fromString
