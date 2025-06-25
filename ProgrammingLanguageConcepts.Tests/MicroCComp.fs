d62826a6e1e7ecbb0e7ae54a2449821bbd0cf95f
teeaychem <bsparkes@stanford.edu>
2025-06-25 11:18:21 +0100

    Partial exercise 8.8, linear search & swap
    No XOR tricks for swap.
    
    I don't think there's too much to gain from the more complex suggestions… though maybe later to relaxation…

--- ProgrammingLanguageConcepts.Tests/MicroCComp.fs
+++ ProgrammingLanguageConcepts.Tests/MicroCComp.fs
@@ -127,3 +127,75 @@ void main() {
 
 
     Assert.Equal("0 1 2", er)
+
+
+[<Fact>]
+let ``Exercise 8.8, linear search`` () =
+
+    let src =
+        @"
+void linsearch(int x, int len, int a[], int *res) {
+  int i;
+  for (i = 0; i < len; ++i) {
+    if (a[i] == x) {
+      *res = i;
+      return;
+    }
+  }
+  *res = len;
+  return;
+}
+
+void main() {
+
+  int arr[3];
+  arr[0] = 3;
+  arr[1] = 1;
+  arr[2] = 2;
+
+  int res;
+
+  int x;
+  for (x = 0; x < 5; ++x) {
+    linsearch(x, 3, arr, &res);
+    print res;
+  }
+}
+"
+
+    let ep = fromString src
+    let er = call_machine ep []
+    let ee = "3 1 2 0 3"
+
+    Assert.Equal(ee, er)
+
+[<Fact>]
+let ``Exercise 8.8, swap`` () =
+
+    let src =
+        @"
+void swap(int *x, int *y) {
+  int tmp;
+  tmp = *x;
+  *x = *y;
+  *y = tmp;
+}
+
+void main() {
+
+  int i; i = 0;
+  int j; j = 1;
+
+  print i; print j;
+
+  swap(&i, &j);
+
+  print i; print j;
+}
+"
+
+    let ep = fromString src
+    let er = call_machine ep []
+    let ee = "0 1 1 0"
+
+    Assert.Equal(ee, er)
