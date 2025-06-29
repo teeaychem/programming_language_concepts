2940e147ed5b2c692872034179917e5118324fc1
teeaychem <bsparkes@stanford.edu>
2025-06-29 14:20:53 +0100

    Exercise 11.2

--- ProgrammingLanguageConcepts.Tests/Cont.fs
+++ ProgrammingLanguageConcepts.Tests/Cont.fs
@@ -22,3 +22,28 @@ let ``Exercise 11.1`` () =
         | _ :: xs -> leni xs 1 + acc
 
     Assert.Equal(3, leni [ 2; 5; 7 ] 0)
+
+
+
+[<Fact>]
+let ``Exercise 11.2`` () =
+
+    let rec revc (xs: 'a list) (k: 'a list -> 'a list) : 'a list =
+        match xs with
+        | [] -> k []
+        | x :: xs -> revc xs (fun v -> x :: k v)
+
+    let result = sprintf "%A" (revc [ 2; 5; 7 ] id)
+    Assert.Equal("[7; 5; 2]", result)
+
+    let result = sprintf "%A" (revc [ 2; 5; 7 ] (fun v -> v @ v))
+    Assert.Equal("[7; 5; 2]", result)
+
+
+    let rec revi (xs: 'a list) acc : 'a list =
+        match xs with
+        | [] -> acc
+        | x :: xs -> revc xs (fun v -> x :: acc)
+
+    let result = sprintf "%A" (revi [ 2; 5; 7 ] [])
+    Assert.Equal("[7; 5; 2]", result)
