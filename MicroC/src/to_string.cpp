391ec54286e9050013ee4556c646495ef1d91f75
teeaychem <ben@sparkes.uk.net>
2025-07-19 16:19:20 +0100

    Misc style changes

--- MicroC/src/to_string.cpp
+++ MicroC/src/to_string.cpp
@@ -1,9 +1,9 @@
 #include <cstdio>
+#include <format>
 #include <memory>
 #include <optional>
 #include <sstream>
 #include <string>
-#include <format>
 
 #include "AST/AST.hpp"
 
@@ -60,27 +60,16 @@ std::string AST::Access::Var::to_string(size_t indent) const {
 std::string AST::Dec::Fn::to_string(size_t indent) const {
   std::stringstream fn_ss{};
 
-  fn_ss << r_typ->to_string(indent)
-        << " "
-        << this->id
-        << " "
-        << "(";
+  fn_ss << std::format("{} {} (", r_typ->to_string(indent), this->id);
 
   if (!params.empty()) {
     for (auto &p : params) {
-      fn_ss << p.first->to_string(indent)
-            << " "
-            << p.second
-            << ", ";
+      fn_ss << std::format("{} {},", p.first->to_string(indent), p.second);
     }
-
     fn_ss.seekp(-2, std::ios_base::end);
   }
 
-  fn_ss << ")"
-        << " "
-        << this->body->to_string(indent)
-        << std::string(indent, ' ');
+  fn_ss << std::format(") {}", this->body->to_string(indent));
 
   return fn_ss.str();
 }
@@ -105,8 +94,7 @@ std::string AST::Expr::Call::to_string(size_t indent) const {
 
   if (!arguments.empty()) {
     for (auto &param : arguments) {
-      call_ss << param->to_string(indent)
-              << ", ";
+      call_ss << param->to_string(indent) << ", ";
     }
     call_ss.seekp(-2, std::ios_base::end);
   }
@@ -141,14 +129,11 @@ std::string AST::Stmt::Block::to_string(size_t indent) const {
 
   std::stringstream block_ss{};
 
-  block_ss << "{"
-           << "\n";
+  block_ss << "{" << "\n";
 
   size_t updated_offset = indent + INDENT_SIZE;
   auto variant_out = [&block_ss, &indent, &updated_offset](const auto v) {
-    block_ss << std::string(updated_offset, ' ')
-             << v->to_string(updated_offset)
-             << "\n";
+    block_ss << std::string(updated_offset, ' ') << v->to_string(updated_offset) << "\n";
   };
 
   for (auto &fresh_var : block.fresh_vars) {
@@ -178,19 +163,18 @@ std::string AST::Stmt::Expr::to_string(size_t indent) const {
 
 std::string AST::Stmt::If::to_string(size_t indent) const {
   std::stringstream if_ss{};
-  if_ss << "if"
-        << " "
-        << this->condition->to_string(indent)
-        << " "
-        << this->stmt_then->to_string(indent);
+  if_ss << std::format("if {} {}", this->condition->to_string(indent), this->stmt_then->to_string(indent));
 
   if (this->stmt_else->kind() == AST::Stmt::Kind::Block) {
     auto as_block = std::static_pointer_cast<AST::Stmt::Block>(this->stmt_else);
-    if (!as_block->block.empty()) {
-      if_ss << " else "
-            << this->stmt_else->to_string(indent);
+    if (as_block->block.empty()) {
+      goto complete_if_string;
     }
   }
+
+  if_ss << std::format(" else {}", this->stmt_else->to_string(indent));
+
+complete_if_string:
 
   return if_ss.str();
 }
@@ -210,10 +194,8 @@ std::string AST::Stmt::Return::to_string(size_t indent) const {
 std::string AST::Stmt::While::to_string(size_t indent) const {
 
   std::stringstream while_ss{};
-  while_ss << "while"
-           << " "
-           << this->condition->to_string(indent)
-           << " "
+  while_ss << "while" << " "
+           << this->condition->to_string(indent) << " "
            << this->stmt->to_string(indent);
 
   return while_ss.str();
