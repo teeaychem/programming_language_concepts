a10a244bb99a35bdafb3ed6d58dac1c5482b507e
teeaychem <bsparkes@stanford.edu>
2025-07-19 15:38:01 +0100

    Fmt files
    Mentioned two commits prior but not included.
    Contains functionality for formatting some custom enums.

---
+++ MicroC/src/AST/Fmt.cpp
@@ -0,0 +1,41 @@
+#include "Fmt.hpp"
+
+auto std::formatter<AST::Typ::Data>::format(AST::Typ::Data c, std::format_context &ctx) const
+    -> std::format_context::iterator {
+  string_view name = "unknown";
+  switch (c) {
+  case AST::Typ::Data::Int: {
+    name = "int";
+  } break;
+  case AST::Typ::Data::Char: {
+    name = "char";
+  } break;
+  case AST::Typ::Data::Void: {
+    name = "void";
+  } break;
+  }
+  return std::formatter<string_view>::format(name, ctx);
+}
+
+auto std::formatter<AST::Stmt::Kind>::format(AST::Stmt::Kind c, std::format_context &ctx) const
+    -> std::format_context::iterator {
+  string_view name = "unknown";
+  switch (c) {
+  case AST::Stmt::Kind::Block: {
+    name = "Block";
+  } break;
+  case AST::Stmt::Kind::Expr: {
+    name = "Expr";
+  } break;
+  case AST::Stmt::Kind::If: {
+    name = "If";
+  } break;
+  case AST::Stmt::Kind::Return: {
+    name = "Return";
+  } break;
+  case AST::Stmt::Kind::While: {
+    name = "While";
+  } break;
+  }
+  return std::formatter<string_view>::format(name, ctx);
+}
