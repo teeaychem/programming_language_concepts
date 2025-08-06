c3c273317b663ce62884ff1b4a324780d24d5f2f
teeaychem <bsparkes@stanford.edu>
2025-08-06 19:16:49 +0100

    Formatting for expr kinds
    Though, mostly redundant...

--- MicroC/src/AST/Fmt.hpp
+++ MicroC/src/AST/Fmt.hpp
@@ -10,6 +10,13 @@ struct std::formatter<AST::Stmt::Kind> : formatter<string_view> {
 };
 
 template <>
+struct std::formatter<AST::Expr::Kind> : formatter<string_view> {
+  auto format(AST::Expr::Kind c, format_context &ctx) const -> format_context::iterator;
+};
+
+
+
+template <>
 struct std::formatter<AST::Expr::OpUnary> : formatter<string_view> {
   auto format(AST::Expr::OpUnary op, format_context &ctx) const -> format_context::iterator;
 };
