ff530b02479e4ad510df71e95f34038c387da9af
teeaychem <ben@sparkes.uk.net>
2025-08-08 15:29:26 +0100

    unify access type generation
    was a todo...
    
    why wasn't this done when creating the type fn?
    I think I assumed the fn was recursive and did a copy, paste, delete from `access` without much reflection...

--- MicroC/src/codegen/LLVMBundle.cpp
+++ MicroC/src/codegen/LLVMBundle.cpp
@@ -6,36 +6,36 @@
 std::pair<llvm::Value *, AST::TypHandle> LLVMBundle::access(AST::ExprT const *expr) {
 
   auto value = expr->codegen(*this);
+  if (value == nullptr) {
+    throw std::logic_error("Unable to load without value information");
+  }
 
   if (expr == nullptr) {
     throw std::logic_error("Unable to load without type information");
   }
 
-  if (value == nullptr) {
-    throw std::logic_error("Unable to load without value information");
-  }
+  auto type = access_type(expr);
 
   switch (expr->kind()) {
 
   case AST::Expr::Kind::Call: {
-    return {value, expr->type()};
+    return {value, type};
   } break;
 
   case AST::Expr::Kind::Cast: {
-    return {value, expr->type()};
+    return {value, type};
   } break;
 
   case AST::Expr::Kind::CstI: {
-    return {value, expr->type()};
+    return {value, type};
   } break;
 
   case AST::Expr::Kind::Index: {
     auto as_index = (AST::Expr::Index *)(expr);
 
-    auto type = as_index->target->type()->deref();
-    auto llvm = this->builder.CreateLoad(type->codegen(*this), value, "acc.idx");
+    value = this->builder.CreateLoad(type->codegen(*this), value, "acc.idx");
 
-    return {llvm, type};
+    return {value, type};
   } break;
 
   case AST::Expr::Kind::Prim1: {
@@ -46,12 +46,11 @@ std::pair<llvm::Value *, AST::TypHandle> LLVMBundle::access(AST::ExprT const *ex
     case AST::Expr::OpUnary::AddressOf: {
       // The access action in this case is *withholding* of a load.
       // And, so, returning the address of the expression.
-      return {value, expr->type()};
+      return {value, type};
     } break;
 
     case AST::Expr::OpUnary::Dereference: {
 
-      auto type = as_prim1->type();
       value = this->builder.CreateLoad(type->codegen(*this), value, "acc.drf");
 
       return {value, type};
@@ -59,13 +58,13 @@ std::pair<llvm::Value *, AST::TypHandle> LLVMBundle::access(AST::ExprT const *ex
 
     case AST::Expr::OpUnary::Sub:
     case AST::Expr::OpUnary::Negation: {
-      return {value, expr->type()};
+      return {value, type};
     } break;
     }
   } break;
 
   case AST::Expr::Kind::Prim2: {
-    return {value, expr->type()};
+    return {value, type};
   } break;
 
   case AST::Expr::Kind::Var: {
@@ -77,18 +76,15 @@ std::pair<llvm::Value *, AST::TypHandle> LLVMBundle::access(AST::ExprT const *ex
     case AST::Typ::Kind::Bool:
     case AST::Typ::Kind::Char:
     case AST::Typ::Kind::Int: {
-      auto type = expr->type();
-      auto llvm = this->builder.CreateLoad(type->codegen(*this), value, as_var->var);
 
-      return {llvm, type};
+      value = this->builder.CreateLoad(type->codegen(*this), value, as_var->var);
+
+      return {value, type};
     } break;
 
     case AST::Typ::Kind::Ptr: {
 
-      auto ptr_typ = std::static_pointer_cast<AST::Typ::Ptr>(expr->type());
-
-      auto type = std::static_pointer_cast<AST::Typ::Ptr>(expr->type());
-      // auto llvm = this->builder.CreateLoad(type->llvm(*this), value, "acc.ptr");
+      auto ptr_typ = std::static_pointer_cast<AST::Typ::Ptr>(type);
 
       if (ptr_typ->area().has_value()) {
         llvm::Value *MC_INT = llvm::ConstantInt::get(llvm::Type::getInt64Ty(*this->context), 0);
@@ -106,10 +102,9 @@ std::pair<llvm::Value *, AST::TypHandle> LLVMBundle::access(AST::ExprT const *ex
     } break;
     }
 
-    auto type = expr->type();
-    auto llvm = this->builder.CreateLoad(type->codegen(*this), value, "acc.vd");
+    value = this->builder.CreateLoad(type->codegen(*this), value, "acc.vd");
 
-    return {llvm, type};
+    return {value, type};
   } break;
   }
 }
