5667c2f453212ab5267bbe9a46f0e5966cc6c53f
teeaychem <bsparkes@stanford.edu>
2025-08-08 21:58:48 +0100

    Simplified access(_type) methods
    Primarily by merging cases.
    Returns have also been unified, and a piece of dead code removed.
    For clarity checks have been removed, as it's highly unlikely they would be effictive.
    In particular, access is intended to be called from a expression, and could be recast as a method on expressions...

--- MicroC/src/codegen/LLVMBundle.cpp
+++ MicroC/src/codegen/LLVMBundle.cpp
@@ -6,53 +6,36 @@
 std::pair<llvm::Value *, AST::TypHandle> LLVMBundle::access(AST::ExprT const *expr) {
 
   auto value = expr->codegen(*this);
-  if (value == nullptr) {
-    throw std::logic_error("Unable to load without value information");
-  }
-
-  if (expr == nullptr) {
-    throw std::logic_error("Unable to load without type information");
-  }
-
   auto type = access_type(expr);
 
   switch (expr->kind()) {
 
+    // Only Index, Prim1, and Var require additional action.
   case AST::Expr::Kind::Call:
   case AST::Expr::Kind::Cast:
-  case AST::Expr::Kind::CstI: {
-    return {value, type};
+  case AST::Expr::Kind::CstI:
+  case AST::Expr::Kind::Prim2: {
   } break;
 
   case AST::Expr::Kind::Index: {
     value = this->builder.CreateLoad(type->codegen(*this), value, "acc.idx");
-    return {value, type};
   } break;
 
+    // Only dereference requires additional action
   case AST::Expr::Kind::Prim1: {
 
     switch (((AST::Expr::Prim1 *)(expr))->op) {
 
-    case AST::Expr::OpUnary::AddressOf: {
-      // The access action in this case is *withholding* of a load.
-    } break;
-
-    case AST::Expr::OpUnary::Dereference: {
-
-      value = this->builder.CreateLoad(type->codegen(*this), value, "acc.drf");
-
-    } break;
-
+    case AST::Expr::OpUnary::AddressOf:
     case AST::Expr::OpUnary::Sub:
     case AST::Expr::OpUnary::Negation: {
     } break;
+
+    case AST::Expr::OpUnary::Dereference: {
+      value = this->builder.CreateLoad(type->codegen(*this), value, "acc.drf");
+    } break;
     }
 
-    return {value, type};
-  } break;
-
-  case AST::Expr::Kind::Prim2: {
-    return {value, type};
   } break;
 
   case AST::Expr::Kind::Var: {
@@ -64,21 +47,19 @@ std::pair<llvm::Value *, AST::TypHandle> LLVMBundle::access(AST::ExprT const *ex
     case AST::Typ::Kind::Char:
     case AST::Typ::Kind::Int: {
       value = this->builder.CreateLoad(type->codegen(*this), value, as_var->var);
-
-      return {value, type};
     } break;
 
     case AST::Typ::Kind::Ptr: {
       auto ptr_typ = std::static_pointer_cast<AST::Typ::Ptr>(type);
 
+      // Decay a pointer to an array
       if (ptr_typ->area().has_value()) {
         value = this->builder.CreateInBoundsGEP(expr->type()->codegen(*this), value, llvm::ArrayRef(this->get_zero()), "acc.ptr.decay");
-      } else {
+      }
+      // Otherwise, load
+      else {
         value = this->builder.CreateLoad(expr->type()->codegen(*this), value, "acc.ptr");
       }
-
-      return {value, type};
-
     } break;
 
     case AST::Typ::Kind::Void: {
@@ -86,63 +67,49 @@ std::pair<llvm::Value *, AST::TypHandle> LLVMBundle::access(AST::ExprT const *ex
     } break;
     }
 
-    value = this->builder.CreateLoad(type->codegen(*this), value, "acc.vd");
-
-    return {value, type};
   } break;
   }
+
+  return {value, type};
 }
 
+// For most expressions the type accessed is the type associated with the expression.
+// E.g., accessing var of int type returns something of int type.
+// The exceptions are pointer deref through indexing or direct deref.
+// Here, the type is obtained through dereference.
 AST::TypHandle LLVMBundle::access_type(AST::ExprT const *expr) {
 
-  if (expr == nullptr) {
-    throw std::logic_error("Unable to load without type information");
-  }
+  auto typ = expr->type();
 
   switch (expr->kind()) {
 
-  case AST::Expr::Kind::Call: {
-    return expr->type();
-  } break;
-
-  case AST::Expr::Kind::Cast: {
-    return expr->type();
-  } break;
-
-  case AST::Expr::Kind::CstI: {
-    return expr->type();
+  case AST::Expr::Kind::Call:
+  case AST::Expr::Kind::Cast:
+  case AST::Expr::Kind::CstI:
+  case AST::Expr::Kind::Prim2:
+  case AST::Expr::Kind::Var: {
   } break;
 
   case AST::Expr::Kind::Index: {
     auto as_index = (AST::Expr::Index *)(expr);
-    return as_index->target->type()->deref();
+    typ = as_index->target->type()->deref();
   } break;
 
   case AST::Expr::Kind::Prim1: {
     auto as_prim1 = (AST::Expr::Prim1 *)(expr);
 
     switch (as_prim1->op) {
-    case AST::Expr::OpUnary::AddressOf: {
-      return expr->type();
+    case AST::Expr::OpUnary::AddressOf:
+    case AST::Expr::OpUnary::Sub:
+    case AST::Expr::OpUnary::Negation: {
     } break;
 
     case AST::Expr::OpUnary::Dereference: {
-      return as_prim1->expr->type()->deref();
-    } break;
-
-    case AST::Expr::OpUnary::Sub:
-    case AST::Expr::OpUnary::Negation: {
-      return expr->type();
+      typ = as_prim1->expr->type()->deref();
     } break;
     }
   } break;
-
-  case AST::Expr::Kind::Prim2: {
-    return expr->type();
-  } break;
-
-  case AST::Expr::Kind::Var: {
-    return expr->type();
-  } break;
   }
+
+  return typ;
 }
