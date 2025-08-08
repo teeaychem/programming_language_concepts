7d108fff179701007e02eaf10db1ea9c6069def9
teeaychem <bsparkes@stanford.edu>
2025-08-08 16:21:51 +0100

    Notes on stmt codegen

--- MicroC/src/codegen/Stmt.cpp
+++ MicroC/src/codegen/Stmt.cpp
@@ -16,6 +16,15 @@ using namespace llvm;
 
 // Stmt
 
+// Blocks are generated in 'canonical' form:
+//
+// - Fresh declarations: declarations whose name does not occurr in any enclosing scope.
+// - Shadowed declarations: declarations whose name *does* occurr in any enclosing scope.
+// - All other statements
+//
+// Generation of statements stops immediately when a `return` statement is found.
+//
+// Codegen mutates the env with any declarations and restore the env on exit of the block.
 Value *AST::Stmt::Block::codegen(LLVMBundle &bundle) const {
 
   std::vector<std::pair<std::string, Value *>> shadowed_values{};
@@ -52,14 +61,22 @@ Value *AST::Stmt::Block::codegen(LLVMBundle &bundle) const {
   return bundle.stmt_return_val();
 }
 
+// Declaration codegen redirects to the declaration.
 Value *AST::Stmt::Declaration::codegen(LLVMBundle &bundle) const {
   return this->declaration->codegen(bundle);
 }
 
+// Expression codegen redirects to the expression.
 Value *AST::Stmt::Expr::codegen(LLVMBundle &bundle) const {
   return this->expr->codegen(bundle);
 }
 
+// If codegen builds directs control flow to the appropriate block.
+// Here, the `then` block is always generated as part of `if` codegen.
+
+// An `else` block is generated if it is present in the AST.
+//
+// And, control returns to the source block only if it's possible by the semantics of the program.
 Value *AST::Stmt::If::codegen(LLVMBundle &bundle) const {
 
   Function *parent = bundle.builder.GetInsertBlock()->getParent();
@@ -85,7 +102,6 @@ Value *AST::Stmt::If::codegen(LLVMBundle &bundle) const {
   if (!this->stmt_then->returns()) { //
     bundle.builder.CreateBr(block_end);
   }
-  // true_block = bundle.builder.GetInsertBlock(); // update for PHI
 
   if (block_else) {
     parent->insert(parent->end(), block_else);
@@ -95,7 +111,6 @@ Value *AST::Stmt::If::codegen(LLVMBundle &bundle) const {
     if (!this->stmt_else->returns()) {
       bundle.builder.CreateBr(block_end);
     }
-    // false_block = bundle.builder.GetInsertBlock();  // update for PHI
   }
 
   if (block_else == nullptr || !this->stmt_then->returns() || !this->stmt_else->returns()) {
@@ -106,11 +121,15 @@ Value *AST::Stmt::If::codegen(LLVMBundle &bundle) const {
   return bundle.stmt_return_val();
 }
 
+// Non void fns set return storage in the `return_alloca` variable.
+// This variable is empty otherwise.
+//
+// Note, in particular, nested blocks in an fn will all access the same return_alloca.
 Value *AST::Stmt::Return::codegen(LLVMBundle &bundle) const {
 
   if (this->value.has_value()) {
 
-    auto [return_val, return_typ] = bundle.access(this->value.value().get());
+    auto [return_val, _] = bundle.access(this->value.value().get());
 
     if (bundle.return_alloca) {
       bundle.builder.CreateStore(return_val, bundle.return_alloca);
@@ -126,6 +145,7 @@ Value *AST::Stmt::Return::codegen(LLVMBundle &bundle) const {
   return bundle.stmt_return_val();
 }
 
+// While codegen mirrors, mostly, if codegen
 Value *AST::Stmt::While::codegen(LLVMBundle &bundle) const {
   Function *parent = bundle.builder.GetInsertBlock()->getParent();
 
@@ -141,6 +161,7 @@ Value *AST::Stmt::While::codegen(LLVMBundle &bundle) const {
 
   parent->insert(parent->end(), block_loop);
   bundle.builder.SetInsertPoint(block_loop);
+
   this->body->codegen(bundle);
   if (!this->body->returns()) { // If entered, the while body may return...
     bundle.builder.CreateBr(block_cond);
