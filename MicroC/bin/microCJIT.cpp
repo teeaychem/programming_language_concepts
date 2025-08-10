c291049eef8a821da40bf20fe0ccc329673847c1
teeaychem <ben@sparkes.uk.net>
2025-08-10 20:54:54 +0100

    Thing struct to handle most things at a very high level
    For clarity, and tests (tbd)...

--- MicroC/bin/microCJIT.cpp
+++ MicroC/bin/microCJIT.cpp
@@ -18,23 +18,127 @@
 
 #include "Driver.hpp"
 
+// The main thing, bundling most tasks.
+struct Thing {
+  // The source file.
+  std::string source;
+
+  // The argument to be passed to main.
+  int64_t arg = 0;
+
+  // The JIT engine, built after generating IR with `build_execution_engine`.
+  llvm::ExecutionEngine *execution_engine{nullptr};
+
+  // Struct for parsing, and codegen by extension
+  Driver driver{};
+
+  // Initialisation from main
+  Thing(std::string source, int64_t arg) : source(source), arg(arg) {
+  }
+
+  // Print the module to stdout.
+  void print_module() {
+    std::cout << "The module:" << "\n"
+              << "---------" << "\n";
+    this->driver.ctx.module->print(llvm::outs(), nullptr);
+    std::cout << "---------" << "\n";
+  }
+
+  // Print a 'canonical' representation of the source to stdout.
+  // This isn't necessarily parsable souce, but is useful for indirect AST inspection.
+  void print_canonical() {
+    std::cout << "Canonical representation... " << "\n"
+              << "---------" << "\n";
+    std::cout << this->driver.prg_string() << "\n";
+    std::cout << "---------" << "\n"
+              << "\n";
+  }
+
+  // Parse the source to an AST, held in `driver`.
+  void parse() {
+    std::cout << "Parsing... ";
+    this->driver.parse(this->source);
+    std::cout << "OK" << "\n";
+  }
+
+  // Generate LLVM IR for an AST.
+  void generate_ir() {
+    std::cout << "Generating LLVM IR... ";
+    this->driver.generate_ir();
+    std::cout << "OK" << "\n";
+  }
+
+  void verify() {
+    std::cout << "Verifying... ";
+    if (llvm::verifyModule(*this->driver.ctx.module, &llvm::outs())) {
+      llvm::errs() << "Error constructing function!";
+      std::exit(1);
+    }
+    std::cout << "OK" << "\n";
+  }
+
+  void build_execution_engine() {
+    std::cout << "Building execution engine... ";
+    std::string err_str;
+    this->execution_engine = llvm::EngineBuilder(std::move(this->driver.ctx.module))
+                                 .setEngineKind(llvm::EngineKind::JIT)
+                                 .setErrorStr(&err_str)
+                                 .create();
+
+    // std::cout << "Extending global mapping... " << "\n";
+    // for (auto &elem : bundle.driver.llvm.foundation_fn_map) {
+    //   auto fn_ptr = elem.second;
+    //   std::cout << "\tAdded: " << fn_ptr->name << "\n";
+    //   execution_engine->addGlobalMapping(fn_ptr->name, fn_ptr->global_map_addr());
+    // }
+
+    if (!execution_engine) {
+      std::cout << "Failed to construct execution engine: " << err_str << "\n";
+      std::exit(1);
+    } else {
+      std::cout << "OK" << "\n";
+    }
+  }
+
+  //
+  void execute_main() {
+    if (!this->execution_engine) {
+      throw std::logic_error("Execution requires engine");
+    }
+
+    auto main_ptr = this->execution_engine->getFunctionAddress("main");
+    if (!main_ptr) {
+      throw std::logic_error("Failed to identify main fn for JIT");
+    }
+
+    int64_t (*main)(int64_t) = (int64_t (*)(int64_t))main_ptr;
+
+    std::cout << "Executing..." << "\n"
+              << "------" << "\n";
+    int64_t exit_code = main(this->arg);
+    std::cout << "\n"
+              << "------" << "\n"
+              << "Exit code: " << exit_code << "\n";
+  }
+};
+
 int main(int argc, char *argv[]) {
   llvm::InitializeNativeTarget();
   llvm::InitializeNativeTargetAsmParser();
   llvm::InitializeNativeTargetAsmPrinter();
 
-  Driver driver{};
+  bool print_canonical = false;
+  bool print_module = false;
+  bool trace_parsing = false;
+  bool trace_scanning = false;
 
   std::vector<std::string> args{};
 
-  bool print_canonical = false;
-  bool print_module = false;
-
   for (size_t i = 1; i < argc; ++i) {
     if (argv[i] == std::string("-p")) {
-      driver.trace_parsing = true;
+      trace_parsing = true;
     } else if (argv[i] == std::string("-s")) {
-      driver.trace_scanning = true;
+      trace_scanning = true;
     } else if (argv[i] == std::string("-c")) {
       print_canonical = true;
     } else if (argv[i] == std::string("-m")) {
@@ -49,77 +153,35 @@ int main(int argc, char *argv[]) {
     std::exit(-1);
   }
 
-  std::cout << "Parsing... ";
-  driver.parse(args[0]);
-  std::cout << "OK" << "\n";
-
-  if (print_canonical) { // AST inspection
-    std::cout << "Canonical representation... " << "\n"
-              << "---------" << "\n"
-              << driver.prg_string() << "\n"
-              << "---------" << "\n"
-              << "\n";
-  }
-
-  std::cout << "Generating LLVM IR... ";
-  driver.generate_llvm();
-  std::cout << "OK" << "\n";
-
-  if (print_module) {
-    std::cout << "The module:" << "\n"
-              << "---------" << "\n";
-    driver.ctx.module->print(llvm::outs(), nullptr);
-    std::cout << "---------" << "\n";
-  }
-
-  std::cout << "Verifying... ";
-  if (llvm::verifyModule(*driver.ctx.module, &llvm::outs())) {
-    llvm::errs() << argv[0] << ": Error constructing function!";
-    return 1;
-  }
-  std::cout << "OK" << "\n";
-
-  std::cout << "Building execution engine... ";
-  std::string err_str;
-  llvm::ExecutionEngine *execution_engine = llvm::EngineBuilder(std::move(driver.ctx.module))
-                                                .setEngineKind(llvm::EngineKind::JIT)
-                                                .setErrorStr(&err_str)
-                                                .create();
-
-  // std::cout << "Extending global mapping... " << "\n";
-  // for (auto &elem : driver.llvm.foundation_fn_map) {
-  //   auto fn_ptr = elem.second;
-  //   std::cout << "\tAdded: " << fn_ptr->name << "\n";
-  //   execution_engine->addGlobalMapping(fn_ptr->name, fn_ptr->global_map_addr());
-  // }
-
-  if (!execution_engine) {
-    std::cout << "Failed to construct execution engine: " << err_str << "\n";
-    return 1;
-  } else {
-    std::cout << "OK" << "\n";
-  }
-
-  auto main_ptr = execution_engine->getFunctionAddress("main");
-  if (!main_ptr) {
-    throw std::logic_error("Failed to identify main fn for JIT");
-  }
+  std::string source = args[0];
 
   // For simplificty, always pass a default arg, as it'll be ignored if not needed.
   int64_t arg = 0;
   if (1 < args.size()) {
     arg = std::stoll(args[1]);
   }
+
   if (2 < args.size()) {
     std::cout << "Note: Only zero or one arguments are supported, all others are ignored..." << "\n";
   }
 
-  int64_t (*main)(int64_t) = (int64_t (*)(int64_t))main_ptr;
+  Thing thing(source, arg);
 
-  std::cout << "Executing..." << "\n"
-            << "------" << "\n";
-  int64_t exit_code = main(arg);
-  std::cout << "\n"
-            << "------" << "\n"
-            << "Exit code: " << exit_code << "\n";
+  thing.parse();
+
+  if (print_canonical) {
+    thing.print_canonical();
+  }
+
+  thing.generate_ir();
+
+  if (print_module) {
+    thing.print_module();
+  }
+
+  thing.verify();
+
+  thing.build_execution_engine();
+
+  thing.execute_main();
 }
